c1b741e5f0b4a633cf3d2a3de1199730193b459b
Felipe Campos <felipecampos50123@gmail.com>
2021-08-09 17:20:36 -0300

    Variáveis

---
+++ 4- Javascript/Variáveis/variaveis.js
@@ -0,0 +1,18 @@
+/*
+As variáveis são caracterizadas por nomes simbólicos para receberem algum valor.
+
+Existem 3 palavras reservadas para criar uma variável, são elas: var, let e const.
+
+*/
+
+var nome = "Felipe"
+console.log("Nome: " + nome)
+console.log("Tipo da variável nome: " + typeof nome)
+
+let idade = 19
+console.log("Idade: " + idade)
+console.log("Tipo da variável idade: " + typeof idade)
+
+const genero = "Masculino" //A "variável" const após receber algum valor não pode ser alterada devido a ela ser constante.
+console.log("Gênero: " + genero)
+console.log("Tipo da variável genero: " + typeof genero)
