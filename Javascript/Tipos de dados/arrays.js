aa956144ad6ba112a27e1ea9c7e8484b4fe01f11
Felipe Campos <felipecampos50123@gmail.com>
2021-08-10 11:59:26 -0300

    Update arrays

--- Javascript/Tipos de dados/arrays.js
+++ Javascript/Tipos de dados/arrays.js
@@ -11,3 +11,31 @@ console.log([
   "Programador",
   "Brasileiro"
 ])
+
+var produtos = [
+  'Leite',
+  'Café',
+  'Pão',
+  'Manteiga'
+]
+
+console.log(produtos)
+console.log(produtos[0])
+console.log(produtos[2])
+
+var pessoas = [
+  {
+    name: 'Felipe',
+    idade: 19
+  },
+  {
+    name: 'Fulano',
+    idade: 30
+  },
+  {
+    name: 'John',
+    idade: 56
+  }
+]
+
+console.log(pessoas[1].name)
