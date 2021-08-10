9938265830eacd43efdf58989f5a79a32f122e99
Felipe Campos <felipecampos50123@gmail.com>
2021-08-10 13:42:47 -0300

    Funções

---
+++ Javascript/Funções/functions.js
@@ -0,0 +1,25 @@
+/*
+  Função é uma estrutura de dado com finalizade de organização do código
+*/
+
+// Declaração da função
+function funcao(){
+  console.log("teste1")
+  console.log("teste2")
+  console.log("teste3")
+}
+
+//A função só irá ser executada caso chamada
+
+funcao()
+
+//Outro exemplo de função
+
+let a = 29
+let b = 15
+
+function soma(a, b){
+  console.log(`Soma entre a e b = ${a + b}`)
+}
+
+soma(a, b)
